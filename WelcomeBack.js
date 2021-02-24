1c6ea46248d107038210bd83f8fa7ae441639e1b
Alumno Informática <alumno.informatica@unealum.local>
2021-02-24 12:14:22 +0100

    commit

--- WelcomeBack.js
+++ WelcomeBack.js
@@ -1,9 +1,17 @@
-class WelcomeBack extends React.component {
+const textillo = "soy un texto";
+const rootEl = document.getElementById("root")
+const spanElement = React.createElement('span', {}, 'SPAN!')
+// const element = React.createElement('h1', { id: 'header' }, 'Hey There!', spanElement)
+
+const element = <h1 id="header">Hola</h1>
+
+class ElementoFunction extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { name: "", appVersion: "" }
     }
     render() {
-        return <h1>Hola</h1>
+        return <h1>{textillo}</h1>
     }
 }
+
+ReactDOM.render(<ElementoFunction />, rootEl)
