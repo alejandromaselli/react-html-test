94310dd0c24b19fad5f86d311eac17d2105968d1
Alumno Informática <alumno.informatica@unealum.local>
2021-02-24 11:56:16 +0100

    inicio

---
+++ WelcomeBack.js
@@ -0,0 +1,9 @@
+class WelcomeBack extends React.component {
+    constructor(props) {
+        super(props)
+        this.state = { name: "", appVersion: "" }
+    }
+    render() {
+        return <h1>Hola</h1>
+    }
+}
